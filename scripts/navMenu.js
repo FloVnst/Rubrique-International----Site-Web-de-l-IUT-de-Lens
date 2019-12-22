45c254aff1083cacdc8f6d852076ad913dd06d98
florianVaneste <florian.vaneste.pro@gmail.com>
2019-12-22 13:56:47 +0100

    ajout menu navigation mobile

---
+++ scripts/navMenu.js
@@ -0,0 +1,18 @@
+hamburgerButton = document.getElementById("hamburgerButton");
+navMenu = document.getElementById("navMenu");
+topBar = document.getElementById("topBar")
+
+function displayNavMenu() {
+    if (window.getComputedStyle(navMenu).getPropertyValue("display") === "none") {
+        navMenu.style.top = window.getComputedStyle(topBar).getPropertyValue("height");
+        navMenu.style.display = "flex";
+        navMenu.style.height = "150px";
+        navMenu.style.width = "100%";
+    }
+    else {
+        navMenu.style.display = "none";
+        navMenu.style.width = "100%";
+    }
+}
+
+hamburgerButton.addEventListener("click", displayNavMenu);
