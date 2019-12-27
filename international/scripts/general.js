ece4f65dafb59e9e3a5702c0f319a25db0bc5d6f
florianVaneste <florian.vaneste.pro@gmail.com>
2019-12-27 02:49:58 +0100

    ajout sections Formation + Contact

--- international/scripts/general.js
+++ international/scripts/general.js
@@ -2,6 +2,8 @@ formationSelectors = [document.getElementById("formationSelectorInformatique"), 
 formations = [document.getElementById("formationsInformatique"), formationsMediasNum = document.getElementById("formationsMediasNum"), document.getElementById("formationsGestion"), document.getElementById("formationsCommerce")];
 displayedFormationCategory = 0;
 
+
+// Attribution d'un identifiant de catégorie de formation en fonction de la catégorie sélectionnée, et ajout de l'écouteur d'évènement sur les sélecteurs de catégorie
 for (i=0; i< formationSelectors.length; i++) {
     formationSelectors[i].addEventListener("click", function () {
         switch (this.id) {
@@ -24,6 +26,7 @@ for (i=0; i< formationSelectors.length; i++) {
     });
 }
 
+// Affichage des formations correspondant à la catégorie sélectionnée
 function displayFormation (formation) {
     formationSelectors[formation].classList.add("selectedFormationSelector");
     formationSelectors[displayedFormationCategory].classList.remove("selectedFormationSelector");
